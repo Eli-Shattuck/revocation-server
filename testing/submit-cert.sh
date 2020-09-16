f020f44e1d350837833829f667d69006b8d14fdc
Jeremy Bennett <jeremy.bennett@uconn.edu>
2020-09-16 13:59:15 -0400

    move files

---
+++ testing/submit-cert.sh
@@ -0,0 +1,12 @@
+#url="http://localhost:8080/new-ct/add-revocation"
+
+#curl -X POST -H "Content-Type: application/json" -d '{"Serial": "09"}' $url 
+
+
+url="http://localhost:8080/new-ct/get-sth"
+curl $url
+
+
+#url="http://localhost:8080/new-ct/get-inclusion-proof"
+
+#curl -X GET -H "Content-Type: application/json" -d '{"Serial": "09"}' $url 
